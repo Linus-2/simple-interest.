c314fd916566a986ea24c5159ed43b826ac26b71
Linus-2 <165094098+Linus-2@users.noreply.github.com>
2024-10-18 07:39:44 +0300

    Add files via upload

---
+++ arrange.C
@@ -0,0 +1,10 @@
+// arrays in c -1D arrays
+#include <stdio.h>
+int main (){
+	int i;
+	int marks[5]={50,90,60,40,70};
+	for(i=0;i<5;i++){
+					printf("marks[%d]=%d \n",i,marks[i]);
+			}
+	return 0;
+}
